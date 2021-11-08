8e5ca6f665490d14d8085a9b8f90cd77ba476dc9
julysein723 <julysein723@gmail.com>
2021-11-08 19:09:22 +0900

    feat : 로그인 화면 우측에 banner 추가

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import LoginProvider from "./LoginProvider";
 import LoginContainer from "./LoginContainer";
+import HomeBanner from "../Home/Home__Banner";
+import { HeaderPageContainer } from "../../Styles/StyledComponents/StyledComponents";
 
 export default function Login() {
     return (
-        <LoginProvider>
-            <LoginContainer />
-        </LoginProvider>
+        <>
+            <LoginProvider>
+                <LoginContainer />
+            </LoginProvider>
+            <BannerContainer>
+                <HomeBanner background_image={false}/>
+            </BannerContainer>
+        </>
     );
 }
+
+const BannerContainer = styled(HeaderPageContainer)`
+    position : absolute;
+    top : 20px;
+    right : 100px;
+`;
